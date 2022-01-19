9d152b48f7cccb07d5851593a81ca4f3de3e3101
Iago da Silva Alves <53544551+IagoAlves777@users.noreply.github.com>
2022-01-19 10:34:18 -0300

    Formulario mult-etapa em react utlizando typescript e styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import { FormProvider } from "./contexts/FormContext";
 
-function App() {
+import { MainRouter } from "./router";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FormProvider>
+      <Router>
+        <MainRouter></MainRouter>
+      </Router>
+    </FormProvider>
+
   );
 }
 
-export default App;
+
+export default App
